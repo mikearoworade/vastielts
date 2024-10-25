91897f766e2540045c41a737d2b02d966cb61cb8
Michael Aroworade <Ayoaro85@gmail.com>
2024-10-25 05:25:45 +0100

    PHP Vastielts

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 require_once(__DIR__ . '/../config/config.php');
 require_once(__DIR__ . '/../config/database.php');
 
-// include(__DIR__. '/../views/shared/admin-header.php');
-// include(__DIR__ . '/../views/shared/admin-footer.php');
+
 include ROOT_PATH . '/views/shared/admin-header.php';
 include ROOT_PATH . '/views/shared/admin-footer.php';
